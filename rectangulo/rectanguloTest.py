9f2a35a44b3b541cb3bb67296f70f17175b3523d
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-04-08 18:00:21 +0200

    Test superposicion FALLA

--- rectangulo/rectanguloTest.py
+++ rectangulo/rectanguloTest.py
@@ -1,7 +1,7 @@
 import unittest
 import rectangulo
-from rectangulo import Rectangulo, obtener_x_y, calcular_area, calcular_perimetro, calcular_centro, obtener_diagonal
-
+from rectangulo import Rectangulo, obtener_x_y, calcular_area, calcular_perimetro, calcular_centro
+from rectangulo import obtener_diagonal, seSuperponen
 """
     Capaz de calcular su área
     Capaz de calcular su perímetro
@@ -35,6 +35,11 @@ class TestRectangulo(unittest.TestCase):
         x_diagonal, y_diagonal = obtener_diagonal(r)
         self.assertEqual(x_diagonal, 1)
         self.assertEqual(y_diagonal, 6)
+
+    def test_superposicion(self):
+        r1 = Rectangulo(1,4,1,6)
+        r2 = Rectangulo(3,5,6,7)
+        self.assertEqual(seSuperponen(r1,r2))
 
 
 if __name__ == '__main__':
