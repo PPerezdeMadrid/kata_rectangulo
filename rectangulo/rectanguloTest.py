c656eb64dbaee1ea4a990b165ff63958d0678843
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-04-08 17:47:53 +0200

    Test perimetro FALLA

--- rectangulo/rectanguloTest.py
+++ rectangulo/rectanguloTest.py
@@ -12,6 +12,11 @@ class TestRectangulo(unittest.TestCase):
         area = calcular_area(x,y)
         self.assertEqual(area, 6)
 
+    def test_calcular_perimetro(self):
+        r = Rectangulo(2, 4, 4, 7)
+        x, y = obtener_x_y(r.xmin, r.xmax, r.ymin, r.ymax)
+        perimetro = calcular_perimetro(x, y)
+        self.assertEqual(perimetro, 2+2+3+3)
 
 
 if __name__ == '__main__':
