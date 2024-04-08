c201d4e86ec2b0f566432a0827e5e90ff71af059
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-04-08 17:52:16 +0200

    test centro FALLA

--- rectangulo/rectanguloTest.py
+++ rectangulo/rectanguloTest.py
@@ -1,9 +1,14 @@
 import unittest
 import rectangulo
-from rectangulo import Rectangulo, obtener_x_y, calcular_area, calcular_perimetro
+from rectangulo import Rectangulo, obtener_x_y, calcular_area, calcular_perimetro, obtener_centro
 
-
-# La clase de prueba
+"""
+    Capaz de calcular su área
+    Capaz de calcular su perímetro
+    Capaz de calcular su centro
+    Capaz de calcular su diagonal
+    Capaz de comprobar si se superpone con otro rectángulo (defina el criterio de superposición)
+    """
 class TestRectangulo(unittest.TestCase):
 
     def test_calcular_area(self):
@@ -17,6 +22,13 @@ class TestRectangulo(unittest.TestCase):
         x, y = obtener_x_y(r.xmin, r.xmax, r.ymin, r.ymax)
         perimetro = calcular_perimetro(x, y)
         self.assertEqual(perimetro, 2+2+3+3)
+
+    def test_calcular_centro(self):
+        # la mitad de x y la mitad de y
+        r = Rectangulo(1, 4, 2, 6)
+        x_centro, y_centro = obtener_centro(r.xmin, r.xmax, r.ymin, r.ymax)
+        self.assertEqual(x_centro, 2.5)
+        self.assertEqual(y_centro, 4.0)
 
 
 if __name__ == '__main__':
